3b7acf20df8a324ca36b99dbff4ca642e3ca239b
Soban77 <sobanamjad87@gmail.com>
2026-05-13 00:34:35 +0500

    changed default PORT to 5000

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,7 @@ const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 // Security middleware
 app.use(helmet({
